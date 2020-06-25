fbcb2cbbff2a28f211c99e9802e1737d3c21e4e6
land-pack <landpack@sina.com>
2020-06-25 23:54:45 +0800

    add rect

--- facemock/core/s.py
+++ facemock/core/s.py
@@ -1,16 +1,17 @@
-import numpy as np 
+import numpy as np
 import cv2
 
-def mark_mouse(filename, position):
-	img=cv2.imread(filename)
-	print("img origin size: {}".format(img.size))
-	img = cv2.resize(img, (800, 800), interpolation=cv2.INTER_CUBIC)
-	
-	cv2.rectangle(img, pos, (pos[0] + 20, pos[1] + 30),(0,255,0),3)
-	#cv2.line(img,(200,0),(111,511),(255,255,2),5)
-	cv2.imwrite(filename, img)
-
+def mark_mouse(filename, rect):
+    # rect = {'height': 44.0, 'width': 548.0, 'x': 313.0, 'y': 228.0}
+    img=cv2.imread(filename)
+    print("img origin size: {}".format(img.size))
+    # img = cv2.resize(img, (800, 800), interpolation=cv2.INTER_CUBIC)
+    pos = int(rect.get("x")), int(rect.get("y"))
+    dia = int(rect.get("x") + rect.get("width")), int(rect.get("y") + rect.get("height"))
+    cv2.rectangle(img, pos, dia,(20,0,250),1)
+    #cv2.line(img,(200,0),(111,511),(255,255,2),5)
+    cv2.imwrite(filename, img)
 
 if __name__ == '__main__':
-	pos = (80, 20)
-	mark_mouse('./sx.png', pos)
+    rect = {'height': 44.0, 'width': 548.0, 'x': 313.0, 'y': 228.0}
+    mark_mouse('./a.png', rect)
